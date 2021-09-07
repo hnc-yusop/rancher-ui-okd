d29c533e7a77b12fca43f0d06b4387abb3e41fe1
YuseopKim <yusop@hancom.com>
2021-09-07 12:21:57 +0900

    Logout Test

--- app/logout/route.js
+++ app/logout/route.js
@@ -5,6 +5,7 @@ export default Route.extend({
   access: service(),
 
   beforeModel(transition) {
+    window.location.href='https://orca-keycloak.cloud.hancom.com/auth/realms/hancom_realm/protocol/openid-connect/logout?redirect_uri=https://orca-rancher.cloud.hancom.com/dashboard/logout';
     transition.send('logout');
   }
 });
